ff4b3631736dac49dd51fb7fe86ff2f097f2fb18
John <John_Maturan0206@outlook.com>
2022-01-03 00:07:04 -0500

    text change

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -37,7 +37,7 @@ export default function Header({ className }) {
               variant="secondary"
               aria-label="Get Started"
             >
-              Call and book
+              Call Now
             </Button>
           </a>
 
